6252ae16bc4ad510c32d6f873456b055a971877d
falling <zhaowenrui_8866@163.com>
2023-04-06 22:37:33 +0800

    Change novel project

--- novelette/novelette/spiders/novelette_spider.py
+++ novelette/novelette/spiders/novelette_spider.py
@@ -1,5 +1,5 @@
 import scrapy
-from ..items import NoveletteItem
+from novelette.items import NoveletteItem
 from scrapy.http import HtmlResponse
 from scrapy import Selector
 from scrapy import Request
@@ -7,30 +7,31 @@ from scrapy import Request
 
 class NoveletteSpiderSpider(scrapy.Spider):
     name = 'novelette_spider'
-    allowed_domains = ['www.scccts.com']
-    start_urls = ['https://www.scccts.com/look/66914428105/']
+    allowed_domains = ['www.xylyc.com']
+    start_urls = ['http://www.xylyc.com/directory/222843/2/']
 
     def parse(self, response: HtmlResponse, **kwargs):
         sel = Selector(response)
-        li_list = sel.css('#list > dl > dd')
-        for chapter_id, li in enumerate(li_list[9:]):
+        li_list = sel.css('#list > dl > a')
+        for chapter_id, li in enumerate(li_list):
             item = NoveletteItem()
             item['chapter_id'] = chapter_id
-            item['title'] = li.css('a::text').extract_first()
+            item['title'] = li.css('dd::text').extract_first()
             item['text'] = ''
-            href = li.css('a::attr(href)').extract_first()
+            href = li.css('::attr(href)').extract_first()
+            print(item['chapter_id'], item['title'])
             yield Request(response.urljoin(href), callback=self.parse_text, cb_kwargs={'item': item})
 
     def parse_text(self, response: HtmlResponse, **kwargs):
         item = kwargs['item']
         sel = Selector(response)
-        for p in sel.css('#content::text'):
+        for p in sel.css('#booktxt > p::text'):
             item['text'] += p.extract().replace('\r', '\n')
-        # next_page = sel.css('#next_url')
-        # if '下一页' in next_page[0].css('::text').extract_first():
-        #     yield Request(response.urljoin(next_page[0].css('::attr(href)').extract_first()),
-        #                   callback=self.parse_text,
-        #                   cb_kwargs={'item': item})
-        # else:
+        next_page = sel.css('#next_url')
+        if '下一页' in next_page[0].css('#next_url').extract_first():
+            item['text'] = item['text'][:-1]
+            yield Request(response.urljoin(next_page[0].css('::attr(href)').extract_first()),
+                          callback=self.parse_text,
+                          cb_kwargs={'item': item})
         print(item['chapter_id'], item['title'])
         yield item
