2801163ece9939185a6b73f8193216ac40a5f89c
falling <zhaowenrui_8866@163.com>
2023-06-22 15:08:53 +0800

    Change novel project

--- novelette/novelette/pipelines.py
+++ novelette/novelette/pipelines.py
@@ -7,13 +7,13 @@
 # useful for handling different item types with a single interface
 class NovelettePipeline:
     def __init__(self):
-        self.f = open(r'.\novels\[综影]纷争女神.txt', 'w+', encoding='utf-8')
+        self.f = open(r'.\novels\穿成人鱼后我又穿了回来.txt', 'w+', encoding='utf-8')
         self.data = {}
         pass
 
     def process_item(self, item, spider):
         self.data[item['chapter_id']] = ''
-        self.data[item['chapter_id']] += item['title']
+        self.data[item['chapter_id']] += f"第{item['chapter_id'] + 1}章 {item['title']}"
         self.data[item['chapter_id']] += '\n'
         self.data[item['chapter_id']] += item['text']
         self.data[item['chapter_id']] += '\n\n'
--- novelette/novelette/spiders/novelette_spider.py
+++ novelette/novelette/spiders/novelette_spider.py
@@ -8,17 +8,17 @@ from scrapy import Request
 
 class NoveletteSpiderSpider(scrapy.Spider):
     name = 'novelette_spider'
-    allowed_domains = ['www.51xs.cc']
+    allowed_domains = ['www.xcbiquge.org']
 
     def start_requests(self):
         for i in range(1):
             # print(f'page: {i + 1}')
-            yield Request(url=f'https://www.51xs.cc/10_38631/', callback=self.parse,
+            yield Request(url=f'http://www.xcbiquge.org/72_72601', callback=self.parse,
                           cb_kwargs={'page': i})
 
     def parse(self, response: HtmlResponse, **kwargs):
         sel = Selector(response)
-        li_list = sel.css('#list > dl > dd > a')[9:]
+        li_list = sel.css('body > div.container.border3-2.mt8.mb20 > div.info-chapters.flex.flex-wrap > a')
         for chapter_id, li in enumerate(li_list):
             item = NoveletteItem()
             item['chapter_id'] = chapter_id + 20 * kwargs['page']
@@ -34,15 +34,15 @@ class NoveletteSpiderSpider(scrapy.Spider):
     def parse_text(self, response: HtmlResponse, **kwargs):
         item = kwargs['item']
         sel = Selector(response)
-        for p in sel.css('#content::text'):
+        for p in sel.css('#article > p::text'):
             text = p.extract().replace('\n', '').replace('\r', '').replace(' ', ' ')
             if not text:
                 continue
             item['text'] += text + '\n'
-        # next_page = sel.css('#list > dl > dd:nth-child(12) > a')
-        # if '下一页' in next_page.extract_first():
-        #     yield Request(response.urljoin(next_page[0].css('::attr(href)').extract_first()),
-        #                   callback=self.parse_text,
-        #                   cb_kwargs={'item': item})
+        next_page = sel.css('#next_url')
+        if '下一页' in next_page.extract_first():
+            yield Request(response.urljoin(next_page[0].css('::attr(href)').extract_first()),
+                          callback=self.parse_text,
+                          cb_kwargs={'item': item})
         print(item['chapter_id'], item['title'])
         yield item
